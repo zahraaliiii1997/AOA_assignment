e4425db2d3ddeb1de9ba0e6197e2e98adec76b9f
zahraaliiii1997 <zahraaliiii1997@gmail.com>
2021-01-17 17:35:23 +0500

    update

--- merge.py
+++ merge.py
@@ -1,38 +1,44 @@
-# ---------------------------------------
-# Merge Sort
-# ---------------------------------------
-import sys
+def merge_sort(array):
+
+    if len(array) <= 1:
+        return array
+
+    midpoint = int(len(array) / 2)
+
+    left, right = merge_sort(array[:midpoint]), merge_sort(array[midpoint:])
+
+    return merge(left, right)
 
 
-def merge_sort(A):
-    merge_sort2(A, 0, len(A) - 1)
+def merge(left, right):
 
+    result = []
+    left_pointer = right_pointer = 0
 
-def merge_sort2(A, first, last):
-    if first < last:
-        middle = (first + last) // 2
-        merge_sort2(A, first, middle)
-        merge_sort2(A, middle + 1, last)
-        merge(A, first, middle, last)
+    while left_pointer < len(left) and right_pointer < len(right):
 
+        if left[left_pointer] < right[right_pointer]:
 
-def merge(A, first, middle, last):
-    L = A[first:middle + 1]
-    R = A[middle + 1:last + 1]
-    L.append(sys.maxsize)
-    R.append(sys.maxsize)
-    i = j = 0
+            result.append(left[left_pointer])
+            left_pointer += 1
 
-    for k in range(first, last + 1):
-        if L[i] <= R[j]:
-            A[k] = L[i]
-            i += 1
         else:
-            A[k] = R[j]
-            j += 1
+
+            result.append(right[right_pointer])
+            right_pointer += 1
+
+    result.extend(left[left_pointer:])
+    result.extend(right[right_pointer:])
+
+    return result
 
 
-A = [5, 9, 1, 2, 4, 8, 6, 3, 7]
-print(A)
-merge_sort(A)
-print(A)
+def main():
+    array = [5, 4, 3, 2, 1]
+    print(array)
+
+    result = merge_sort(array)
+    print(result)
+
+if __name__ == "__main__":
+    main()
